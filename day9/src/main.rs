055a73f947818abf1135b0fbaaa35e69d699d854
Adam Chalmers <adam.s.chalmers@gmail.com>
2022-12-10 20:51:05 -0600

    Day 9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,8 +2,8 @@ use std::collections::HashSet;
 
 fn main() {
     let moves = parse(include_str!("../input"));
-    let a1 = simulate(&moves);
-    println!("Q1: {a1}");
+    println!("Q1: {}", simulate(&moves, false, 2));
+    println!("Q2: {}", simulate(&moves, false, 10));
 }
 
 #[derive(Default, Debug, PartialEq, Eq, Hash, Clone, Copy)]
@@ -13,32 +13,117 @@ struct Point {
 }
 
 impl Point {
-    fn move_by(self, Move { dir, n }: &Move) -> Self {
-        let Self { mut x, mut y } = self;
+    #[must_use]
+    fn move_dir(mut self, dir: Dir) -> Self {
         match dir {
-            Dir::U => y += n,
-            Dir::D => y -= n,
-            Dir::L => x += n,
-            Dir::R => x -= n,
+            Dir::U => self.y += 1,
+            Dir::D => self.y -= 1,
+            Dir::L => self.x -= 1,
+            Dir::R => self.x += 1,
         }
-        Self { x, y }
+        self
     }
 
     // Move this point, the tail, towards the given head.
-    fn move_towards(self, head: Self) -> Self {}
+    #[must_use]
+    fn move_towards(self, head: &Self) -> Self {
+        let mut tail = self;
+
+        // If they're touching, no move is necessary.
+        // Diagonally adjacent and even overlapping both count as touching
+        if head.x.abs_diff(tail.x) <= 1 && head.y.abs_diff(tail.y) <= 1 {
+            return tail;
+        }
+
+        // If the head is ever two steps directly up, down, left, or right from the tail, the tail
+        // must also move one step in that direction so it remains close enough.
+        if head.x == tail.x {
+            if head.y == tail.y + 2 {
+                tail.y += 1;
+                return tail;
+            }
+            if head.y == tail.y - 2 {
+                tail.y -= 1;
+                return tail;
+            }
+        }
+        if head.y == tail.y {
+            if head.x == tail.x + 2 {
+                tail.x += 1;
+                return tail;
+            }
+            if head.x == tail.x - 2 {
+                tail.x -= 1;
+                return tail;
+            }
+        }
+
+        // Otherwise, move diagonally towards the head.
+
+        // Is tail's x too small?
+        if head.x > tail.x {
+            tail.x += 1;
+        } else {
+            tail.x -= 1;
+        }
+        // Is tail's y too small?
+        if head.y > tail.y {
+            tail.y += 1;
+        } else {
+            tail.y -= 1;
+        }
+
+        tail
+    }
 }
 
-/// Returns number of positions the tail of the rope visits at least once
-fn simulate(moves: &[Move]) -> usize {
+/// Returns number of positions the tail of the rope visits at least once.
+/// Simulates `n` points.
+fn simulate(moves: &[Move], print_steps: bool, knots: usize) -> usize {
     let mut points_visited = HashSet::new();
-    let mut head = Point::default();
-    let mut tail = Point::default();
+    // The head is #0, the tail is #n.
+    let mut points = vec![Point::default(); knots];
     for mv in moves {
-        points_visited.insert(tail);
-        head = head.move_by(mv);
-        tail = tail.move_towards(head);
+        if print_steps {
+            mv.print();
+        }
+        for _step in 0..mv.steps {
+            points[0] = points[0].move_dir(mv.dir);
+            for i in 1..points.len() {
+                let head = points[i - 1];
+                let tail = points[i];
+                points[i] = tail.move_towards(&head);
+                if print_steps {
+                    print_grid(points[i - 1], points[i]);
+                }
+            }
+            points_visited.insert(points.last().unwrap().to_owned());
+        }
     }
     points_visited.len()
+}
+
+fn print_grid(head: Point, tail: Point) {
+    const WIDTH: usize = 6;
+    for y in (0..WIDTH).rev() {
+        for x in 0..WIDTH {
+            let p = Point {
+                x: x as _,
+                y: y as _,
+            };
+            if p == Default::default() {
+                print!("s");
+            } else if p == head {
+                print!("H");
+            } else if p == tail {
+                print!("T");
+            } else {
+                print!(".")
+            }
+        }
+        println!();
+    }
+    println!()
 }
 
 #[derive(Clone, Copy)]
@@ -49,10 +134,28 @@ enum Dir {
     R,
 }
 
+impl std::fmt::Display for Dir {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            Dir::U => "U",
+            Dir::D => "D",
+            Dir::L => "L",
+            Dir::R => "R",
+        };
+        s.fmt(f)
+    }
+}
+
 #[derive(Clone, Copy)]
 struct Move {
     dir: Dir,
-    n: isize,
+    steps: isize,
+}
+
+impl Move {
+    fn print(&self) {
+        println!("== {} {} ==", self.dir, self.steps);
+    }
 }
 
 fn parse(input: &str) -> Vec<Move> {
@@ -60,7 +163,7 @@ fn parse(input: &str) -> Vec<Move> {
         .lines()
         .map(|line| line.split_once(' ').unwrap())
         .map(|(dir, distance)| Move {
-            n: distance.parse::<u8>().unwrap() as _,
+            steps: distance.parse::<u8>().unwrap() as _,
             dir: match dir {
                 "D" => Dir::D,
                 "U" => Dir::U,
@@ -80,5 +183,21 @@ mod tests {
     fn test_parse() {
         let input = include_str!("../input");
         assert_eq!(2000, parse(input).len());
+    }
+
+    #[test]
+    fn test_example_q1() {
+        let input = include_str!("../example");
+        let moves = parse(input);
+        let q1 = simulate(&moves, true, 2);
+        assert_eq!(q1, 13);
+    }
+
+    #[test]
+    fn test_real_q1() {
+        let input = include_str!("../input");
+        let moves = parse(input);
+        assert_eq!(simulate(&moves, false, 2), 6367);
+        assert_eq!(simulate(&moves, false, 10), 2536);
     }
 }
